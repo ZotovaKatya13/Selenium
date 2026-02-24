20b28d4e3456fc4c1dbb9e5803b8c08042ae67fc
ekaterina <katyaz19981998@mail.ru>
2026-02-24 19:28:09 +0400

    add tests

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -12,6 +12,29 @@ public class LoginTest extends BaseTest{
        loginPage.usernameField.setValue("Admin");
        loginPage.passwordField.setValue("admin123");
        loginPage.loginButton.click();
-
+    }
+    @Test
+    public void loginWithInvalidCreds(){
+        loginPage.usernameField.setValue("Admin");
+        loginPage.passwordField.setValue("wrong");
+        loginPage.loginButton.click();
+    }
+    @Test
+    public void loginWithInvalidUsername(){
+        loginPage.usernameField.setValue("wrong");
+        loginPage.passwordField.setValue("admin123");
+        loginPage.loginButton.click();
+    }
+    @Test
+    public void loginWithEmptyCreds(){
+        loginPage.usernameField.setValue("");
+        loginPage.passwordField.setValue("");
+        loginPage.loginButton.click();
+    }
+    @Test
+    public void loginWithLongUserName(){
+        loginPage.usernameField.setValue("admin123qwertyuio!@#$%^&*()");
+        loginPage.passwordField.setValue("");
+        loginPage.loginButton.click();
     }
 }
