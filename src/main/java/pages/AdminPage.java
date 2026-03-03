146dce83a3447a103af0f9ce4cf2cacf80131578
ekaterina <katyaz19981998@mail.ru>
2026-03-03 12:03:53 +0400

    add new tests

--- src/main/java/pages/AdminPage.java
+++ src/main/java/pages/AdminPage.java
@@ -8,10 +8,16 @@ import static com.codeborne.selenide.Selenide.$;
 public class AdminPage  {
  //   private SelenideElement usernameField = $(By.xpath("//form//input[@class='oxd-input oxd-input--active']"));
     public SelenideElement usernameField = $(By.xpath("//form//input[@class='oxd-input oxd-input--active']"));
-    private SelenideElement searchButton = $(By.xpath("//button[@type='submit']"));
+    // private SelenideElement searchButton = $(By.xpath("//button[@type='submit']"));
+    public SelenideElement searchButton = $(By.xpath("//button[@type='submit']"));
 
     public void searchByUsername(String username) {
         usernameField.setValue(username);
         searchButton.click();
     }
+
+    // селектор
+    public SelenideElement userRoleButton = $(By.xpath("(//div[@class='oxd-select-text oxd-select-text--active'])[1]"));
+    public SelenideElement userRoleDropDown = $(By.xpath("//div[@class='oxd-select-dropdown --positon-bottom']"));
+
 }
--- src/test/java/tests/AdminTest.java
+++ src/test/java/tests/AdminTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import org.testng.annotations.Test;
 import pages.AdminPage;
 import pages.LoginPage;
@@ -13,13 +14,20 @@ public class AdminTest extends BaseTest {
     public void searchAdmin() {
         app.loginPage.login(app.userCreds.adminLogin, app.userCreds.adminPassword);
         app.sideMenu.openTab("admin");
-//        page(AdminPage.class).openAdminTab();
-//        app.adminPage.adminTab.click();
         app.adminPage.searchByUsername("Admin");
     }
+
     @Test
-    public void testDropDown() {
-       app.adminPage.usernameField.click();
-       app.adminPage.usernameField.getWrappedElement().findElement(byText("Москва")).click();
+    public void searchAndDropDownCheck() {
+//       app.adminPage.usernameField.getWrappedElement().findElement(byText("Москва")).click();
+        app.loginPage.login(app.userCreds.adminLogin, app.userCreds.adminPassword);
+        app.sideMenu.openTab("admin");
+        app.adminPage.usernameField.setValue("admin");
+        app.adminPage.userRoleButton.click();
+        // в раскрывающемся списке ищем элемент по тексту
+        app.adminPage.userRoleDropDown.getWrappedElement().findElement(byText("Admin")).click();
+        app.adminPage.userRoleButton.shouldHave(Condition.exactText("Admin"));
+        app.adminPage.searchButton.click();
+
     }
 }
